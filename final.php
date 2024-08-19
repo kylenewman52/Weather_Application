3df9a09059afbdc0cc5cf7cc4be84cb0e66a1433
Kyle Newman <64489588+kylenewman52@users.noreply.github.com>
2024-08-19 16:33:17 -0400

    Update final.php

--- final.php
+++ final.php
@@ -1,9 +1,5 @@
 <?php
-/*
-   scott campbell
-   Kurt Johnson
-   display temps
- */
+
 $DBSTRING = "sqlite:/home/ubuntu/cse383/database/cse383.db";  // This must be updated to your real database location 
 include "sql.inc";
 include "final.class.php";
@@ -13,11 +9,6 @@ header ("Access-Control-Allow-Methods: GET,POST,PUSH,OPTIONS");
 header ("content-type: application/json");
 header ("Access-Control-Allow-Headers: Content-Type");
 require_once "RestServer.php";
-// phpinfo();
-
-// Kurt Johnson
-// with linux function code by Scott Campbell
-
 
 $method=$_REQUEST["method"];
 // example request: http://path/to/resource/rest/api/vi/sayHello?&name=World
